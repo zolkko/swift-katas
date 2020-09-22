c06487be0d5bdfe67f6e5432a0d76afd33349f24
zolkko <zolkko@gmail.com>
2020-09-22 19:48:42 +0300

    Add section 2

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Turtle Rock")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
