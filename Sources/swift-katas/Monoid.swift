08753f8c11830200bfd431b19e6857891b68f25d
zolkko <zolkko@gmail.com>
2020-12-10 20:06:26 +0300

    Add monoid example.

---
+++ Sources/swift-katas/Monoid.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+
+protocol Monoid {
+    static var mempty: Self { get }
+    func op(_ other: Self) -> Self
+}
+
+infix operator <> : AdditionPrecedence
+
+func <> <M: Monoid>(lhs: M, rhs: M) -> M {
+    return lhs.op(rhs)
+}
+
+extension String: Monoid {
+    static var mempty: String = ""
+    func op(_ other: String) -> String {
+        return self + other
+    }
+}
+
+func sample() {
+    /// Usage example
+    print("hello" <> "world")
+}
